3bd9dd46ddf55fe9ab07d00a863ac8eb4fcfcb27
Yushin Si <siyushin@hotmail.com>
2020-10-07 11:45:39 +0800

    updated the abi of XRC20.

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 const Config = {
-	// rpcURL: "http://api.testnet.iotex.one:80",
-	rpcURL: "http://api.iotex.one:80",
+	rpcURL: "http://api.testnet.iotex.one:80",
+	// rpcURL: "http://api.iotex.one:80",
 	ipfsURL: "localhost",
 	ipfsPort: 5001,
 	ipfsScheme: "http"
