26a8a2e39df08d74a0e2ac74215c67fcaa51d5e8
Yushin Si <siyushin@hotmail.com>
2020-10-15 15:36:16 +0800

    Modified Config.js.

--- src/Config.js
+++ src/Config.js
@@ -1,8 +1,8 @@
 const Config = {
 	// rpcURL: "http://api.testnet.iotex.one:80",
 	rpcURL: "https://api.mainnet.iotex.one:443",
-	// ipfsURL: "ipfs.minttoken.io",
-	ipfsURL: "localhost",
+	ipfsURL: "ipfs.minttoken.io",
+	// ipfsURL: "localhost",
 	ipfsPort: 5001,
 	ipfsScheme: "http"
 }
